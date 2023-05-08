f2ae84e5a231ecb0cbd045f4ca671dd59a4b9462
AmzGrainRain <amzgrainrain@outlook.com>
2023-05-08 16:18:33 +0800

    big change!

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const searchList = [
+  '/docs/hadoop/README.md',
+  '/docs/zookeeper/README.md',
+  '/docs/mysql/README.md',
+  '/docs/hive/README.md',
+  '/docs/sqoop/README.md',
+  '/docs/hbase/README.md',
+  '/docs/scala&spark/README.md',
+  '/docs/flume/README.md',
+  '/docs/kafka/README.md',
+  '/docs/sql/README.md',
+  '/docs/sql_adv/README.md',
+  '/docs/scala/README.md',
+  '/docs/c++11/README.md'
+]
+
+window.$docsify = {
+  el: '#app',
+  repo: 'https://github.com/AmzGrainRain/Documents',
+  maxLevel: 6,
+  loadNavbar: true,
+  auto2top: true,
+  coverpage: true,
+  name: '返回首页',
+  nameLink: '/',
+  themeColor: '#000000',
+  noEmoji: false,
+  mergeNavbar: true,
+  cornerExternalLinkTarget: '_blank',
+  // routerMode: 'history',
+  // hideSidebar: true,
+  onlyCover: true,
+  requestHeaders: {
+    'cache-control': 'max-age=600'
+  },
+  ext: '.md',
+  basePath: '/',
+  search: {
+    paths: searchList,
+    placeholder: '全局搜索',
+    noData: '找不到结果',
+    depth: 6
+  }
+}
+
+// Service Worker Register
+if (typeof navigator.serviceWorker !== 'undefined') {
+  navigator.serviceWorker.register('/js/sw.js')
+}
